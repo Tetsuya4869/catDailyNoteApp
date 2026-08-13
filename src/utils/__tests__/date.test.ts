6c8376e0c7d7efdd6fcbef3192171042d020819e
Tetsuya4869 <32119044+Tetsuya4869@users.noreply.github.com>
2026-08-13 14:43:04 +0900

    Mock AsyncStorage in date regression tests

--- src/utils/__tests__/date.test.ts
+++ src/utils/__tests__/date.test.ts
@@ -1,3 +1,7 @@
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
+
 import { dateOnlyToLocalDate, shiftDateOnly, toDateOnly } from '../date';
 import { calculateStreak } from '../../storage/diaryStorage';
 import { DiaryEntry } from '../../types';
